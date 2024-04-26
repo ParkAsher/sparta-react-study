aed183b3e4b8432252d08251be67e55128078f5a
ParkAsher <devasherpark@naver.com>
2024-04-26 23:41:09 +0900

    [feat]: 부모-자식 컴포넌트 연습

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -24,28 +24,42 @@ import 'App.css';
 //     );
 // }
 
-function App() {
-    // <--- 자바스크립트 영역 --->
-    const handleClick = () => {
-        alert('이것은 내가 만든 App 컴포넌트 입니다.');
-    };
+// function App() {
+//     // <--- 자바스크립트 영역 --->
+//     const handleClick = () => {
+//         alert('이것은 내가 만든 App 컴포넌트 입니다.');
+//     };
 
-    return (
-        /* <!--- HTML/JSX 영역 ---> */
-        <div
-            style={{
-                height: '100vh',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            {/* 이곳에 퀴즈를 위한 HTML 코드를 작성해 주세요 */}
-            이것은 내가 만든 App 컴포넌트 입니다.
-            <button onClick={handleClick}>클릭!</button>
-        </div>
-    );
+//     return (
+//         /* <!--- HTML/JSX 영역 ---> */
+//         <div
+//             style={{
+//                 height: '100vh',
+//                 display: 'flex',
+//                 flexDirection: 'column',
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//             }}
+//         >
+//             {/* 이곳에 퀴즈를 위한 HTML 코드를 작성해 주세요 */}
+//             이것은 내가 만든 App 컴포넌트 입니다.
+//             <button onClick={handleClick}>클릭!</button>
+//         </div>
+//     );
+// }
+
+/* 
+    컴포넌트 화 
+    반복 되는 요소들은 타이핑이 길어지므로 컴포넌트 화
+*/
+// 자식 컴포넌트
+function Child() {
+    return <div>나는 자식입니다.</div>;
+}
+
+// 부모 컴포넌트
+function App() {
+    return <Child />;
 }
 
 export default App;

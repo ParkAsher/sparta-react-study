e823c16d06b044ac21142a24f863c35828e2d66b
ParkAsher <devasherpark@naver.com>
2024-06-06 13:59:55 +0900

    [feat] : redux action values & action creators

--- redux-counter-example/src/App.jsx
+++ redux-counter-example/src/App.jsx
@@ -1,24 +1,20 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { plusOne, minusOne } from './redux/modules/counter';
 
 function App() {
     // 여기에서 store에 접근하여, counter의 값을 읽어오고 싶다!
     // useSelector
-
     const counter = useSelector((state) => state.counter);
 
     const dispatch = useDispatch();
 
     const onPlusButtonClickHandler = () => {
-        dispatch({
-            type: 'PLUS_ONE',
-        });
+        dispatch(plusOne());
     };
 
     const onMinusButtonClickHandler = () => {
-        dispatch({
-            type: 'MINUS_ONE',
-        });
+        dispatch(minusOne());
     };
 
     return (
--- redux-counter-example/src/redux/modules/counter.js
+++ redux-counter-example/src/redux/modules/counter.js
@@ -1,3 +1,20 @@
+// action value
+const PLUS_ONE = 'counter/PLUS_ONE';
+const MINUS_ONE = 'counter/MINUS_ONE';
+
+// action creator : action value 를 return 하는 함수
+export const plusOne = () => {
+    return {
+        type: PLUS_ONE,
+    };
+};
+
+export const minusOne = () => {
+    return {
+        type: MINUS_ONE,
+    };
+};
+
 // 초기상태값(state)
 const initialState = {
     number: 0,
@@ -9,11 +26,11 @@ const initialState = {
 // inpunt : state & action
 const counter = (state = initialState, action) => {
     switch (action.type) {
-        case 'PLUS_ONE':
+        case PLUS_ONE:
             return {
                 number: state.number + 1,
             };
-        case 'MINUS_ONE':
+        case MINUS_ONE:
             return {
                 number: state.number - 1,
             };
